14986222aa6bb9cea4188db0aed8bfab35c15f97
I Lin WU <ilinwu@Is-MacBook-Air.broadband>
2021-09-11 23:54:57 +0100

    codes forcrypto-tracker

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,12 +1,43 @@
+import styles from './Coins.module.scss';
+import Link from 'next/link';
 
+const Coins = ({
+        name, 
+        price, 
+        symbol, 
+        marketcap, 
+        volume, 
+        image, 
+        priceChange,
+        id
+    }) => { 
+        return (
+            <Link href='./coin/[id]' as={`/coin/${id}`}>
+             <a>
+                <div className={styles.coin_container}>
+                    <div className={styles.coin_row}>
+                        <div className={styles.coin}>
+                            <img src={image} alt={name} className={styles.coin_img} />
+                                <h1 className={styles.coin_h1}>{name}</h1>
+                                <p className={styles.coin_symbol}>{symbol}</p>
+                        </div>
 
-const Coins = ({name, price}) => {
-    return (
-        <div>
-            <h1>{name}</h1>
-            <p>{price}</p>
-        </div>
-    )
+                        <div className={styles.coin_data}>
+                            <p className={styles.coin_price}>£{price}</p>
+                            <p className={styles.coin_volume}>£{volume.toLocaleString()}</p>
+                            {priceChange < 0 ? 
+                                (<p className={styles.coin_precent, styles.red}>{priceChange.toFixed(2)}%</p>) 
+                                : 
+                                (<p className={styles.coin_precent, styles.green}>{priceChange.toFixed(2)}%</p>)
+                            }
+
+                            <p className={styles.coin_marketcap}>Mkt Cap: £{marketcap.toLocaleString()}</p>
+                        </div>
+                    </div>
+                </div>
+             </a>
+            </Link>
+        )
 }
 
 export default Coins;
--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,4 +1,4 @@
- import styles from './Search.module.css';
+ import styles from './Search.module.scss';
  
  const SearchBar = ({...rest}) => {
      return (
